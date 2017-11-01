1b8a161c3efbd77c8e4678e7bdd93704b0338dc8
Eden Rohatensky <edenrohatensky@gmail.com>
2017-11-01 10:51:37 -0400

    Commit rest of lecture

---
+++ app/models/restaurant.rb
@@ -0,0 +1,7 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  validates :stars, inclusion: { in: 1..3, allow_nil: false }
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    collection do # no id in the url
+      get 'top', to: "restaurants#top" #RestaurantsController#top
+    end
+    member do # member => id in URL
+      get 'chef', to: 'restaurants#chef'
+    end
+
+    resources :reviews, only: [:new, :create]
+  end
+
+  resources :reviews, only: [:edit]
+
+  namespace :admin do
+    resources :restaurants, only: [:index]
+  end
 end
